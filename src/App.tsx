504d2b9a635b6ec7d10dfab76a83282e845d96a1
min <mmbliv@gmail.com>
2023-01-22 22:37:51 -0600

    number input style changed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Form } from "./stories/Form";
 function App() {
   return (
     <div className="App">
-      <Form type="numberInput" />{" "}
+      <Form type="numberInput" step={1} startValue={1} />
     </div>
   );
 }
--- src/stories/Form.tsx
+++ src/stories/Form.tsx
@@ -1,6 +1,6 @@
 import { kMaxLength } from "buffer";
 import { string } from "prop-types";
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./form.css";
 interface FormProps {
   label?: string;
@@ -12,10 +12,10 @@ interface FormProps {
     | "textInputWithButton"
     | "checkBox";
   options?: { [key: number]: string };
-  min?: string;
-  max?: string;
-  step?: string;
-  startValue?: string;
+  min?: number;
+  max?: number;
+  step: number;
+  startValue?: number;
 }
 
 export const Form = ({
@@ -25,9 +25,29 @@ export const Form = ({
   options = { 1: "select" },
   min,
   max,
-  step,
+  step = 1,
   startValue,
 }: FormProps) => {
+  const refNumber = useRef<HTMLInputElement>(null);
+  const increase = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const currenValue = +refNumber.current!.value;
+    if (currenValue < max!) {
+      let n = +refNumber.current!.value + step;
+      refNumber.current!.value = n.toString();
+    } else {
+      return;
+    }
+  };
+
+  const decrease = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const currenValue = +refNumber.current!.value;
+    if (currenValue > min!) {
+      let n = +refNumber.current!.value - step;
+      refNumber.current!.value = n.toString();
+    }
+  };
   const setOptions = () => {
     const optionArr = [];
     for (let i in options) {
@@ -66,17 +86,22 @@ export const Form = ({
 
   if (type === "numberInput")
     return (
-      <form>
+      <form className="number-input">
+        <button onClick={(e) => decrease(e)}></button>
         <input
           type="number"
           id="number"
           name="number"
-          min={min}
-          max={max}
-          step={step}
+          // min={min}
+          // max={max}
+          // step={step}
           defaultValue={startValue}
+          // value={}
+          ref={refNumber}
+          // value={refNumber}
           // onChange={}
         />
+        <button className="plus" onClick={(e) => increase(e)}></button>
       </form>
     );
 
